a628313db20427c1afebabf23fd5b9d8589b47a0
shahviransh <shahv47@mcmaster.ca>
2022-05-10 19:29:49 -0400

    Kinda Functioning Program

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
-from tkinter import *
 import speech_recognition as sr
+import pyttsx3
+import time
+
+r = sr.Recognizer()
+mic = sr.Microphone()
+ 
+def SpeakText(command):
+    engine = pyttsx3.init()
+    newVoiceRate = 145
+    engine.setProperty('rate',newVoiceRate)
+    engine.say(command)
+    engine.runAndWait()
+
+while(1):
+    try:
+        with mic as source:
+            r.adjust_for_ambient_noise(source, duration=0.2)
+            audio2 = r.listen(source,timeout=5)
+             
+            myText = r.recognize_google(audio2)
+            myText = myText.lower()
+            
+            if (myText == "stop"):
+                break
+ 
+            print("Did you say "+ myText + "?")
+            SpeakText(myText)
+             
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+         
+    except sr.UnknownValueError:
+        print("Unknown error occured.")

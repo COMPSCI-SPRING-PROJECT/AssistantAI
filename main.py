c5713843271f23b1a43a1a1ca959bf9c8b11924c
shahviransh <shahv47@mcmaster.ca>
2022-05-10 21:49:17 -0400

    main function added

--- main.py
+++ main.py
@@ -2,8 +2,30 @@ import speech_recognition as sr
 import pyttsx3
 import time
 
-r = sr.Recognizer()
-mic = sr.Microphone()
+def main():
+    r = sr.Recognizer()
+    mic = sr.Microphone()
+
+    while(1):
+        try:
+            with mic as source:
+                r.adjust_for_ambient_noise(source, duration=0.2)
+                audio2 = r.listen(source,timeout=5)
+                
+                myText = r.recognize_google(audio2)
+                myText = myText.lower()
+                
+                if (myText == "stop"):
+                    break
+    
+                print("Did you say "+ myText + "?")
+                SpeakText(myText)
+                
+        except sr.RequestError as e:
+            print("Could not request results; {0}".format(e))
+            
+        except sr.UnknownValueError:
+            print("Unknown error occured.")
  
 def SpeakText(command):
     engine = pyttsx3.init()
@@ -12,23 +34,4 @@ def SpeakText(command):
     engine.say(command)
     engine.runAndWait()
 
-while(1):
-    try:
-        with mic as source:
-            r.adjust_for_ambient_noise(source, duration=0.2)
-            audio2 = r.listen(source,timeout=5)
-             
-            myText = r.recognize_google(audio2)
-            myText = myText.lower()
-            
-            if (myText == "stop"):
-                break
- 
-            print("Did you say "+ myText + "?")
-            SpeakText(myText)
-             
-    except sr.RequestError as e:
-        print("Could not request results; {0}".format(e))
-         
-    except sr.UnknownValueError:
-        print("Unknown error occured.")
+main()

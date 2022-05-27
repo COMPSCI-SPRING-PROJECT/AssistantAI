cd722389592ce844ca794b8483a02d764fbe4dff
leungt30 <leungt30@mcmaster.ca>
2022-05-27 12:43:09 -0400

    Update weather.py

--- weather.py
+++ weather.py
@@ -5,6 +5,15 @@ import requests
 class weather:
 
     KEY = "3fb551e63f9e07cb81cc64ff1bf4fb61" #find a way to hide key
+
+    def __callAPI(self,link):
+        req = requests.get(link)
+        data = req.json()
+        return data
+
+    def __convertKtoC(self, temp):
+        return round(temp-273.15,1)
+
     """
     getWeather takes a location and returns a dictionary
     the dictionary contains whether the API call was successful, if it is successful then it also 
@@ -12,20 +21,15 @@ class weather:
     """
     def getWeather(self, location: str):
         try:
-            geo_encode_link= "http://api.openweathermap.org/geo/1.0/direct?q="+location+"&limit=2&appid="+self.KEY
-            geo_link = requests.get(geo_encode_link)
-            geo_data = geo_link.json()
+            geo_data = self.__callAPI("http://api.openweathermap.org/geo/1.0/direct?q="+location+"&limit=2&appid="+self.KEY)
             lat = str(geo_data[0]['lat'])
             lon = str(geo_data[0]['lon'])
-            complete_api_link = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+self.KEY
-            api_link = requests.get(complete_api_link)
-            api_data = api_link.json()
+            api_data = self.__callAPI("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+self.KEY)
             weather = api_data["weather"][0]['description']
-            temperature = str(round(api_data["main"]['temp']-273.15,1))
-            feels_like = str(round(api_data["main"]["feels_like"]-273.15,1))
+            temperature=str(self.__convertKtoC(api_data["main"]['temp']))
+            feels_like = str(self.__convertKtoC(api_data["main"]["feels_like"]))
             humidity = str(api_data["main"]['humidity'])
-            results = {'success':True ,'weather':weather,'temperature':temperature,'feels like': feels_like,'humidity':humidity}
-            return results
+            return {'success':True ,'weather':weather,'temperature':temperature,'feels like': feels_like,'humidity':humidity}
         except exception:
             return {'success':False}
 

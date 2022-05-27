0481e52d4f3367a4d4939f2319ffbcb4f3f56ec6
UjjwalRaj18 <89405900+UjjwalRaj18@users.noreply.github.com>
2022-05-26 20:43:50 -0400

    commit

---
+++ GUI.py
@@ -0,0 +1,13 @@
+from tkinter import *
+def func():
+    i = Tk()
+    Name = i.title("Avery")
+    greetings = Label(text="How are you",fg="white",bg="black")
+    button = Button(text="Speak",width=35,height=8,bg="yellow",fg="blue")
+    greetings.pack()
+    button.pack()
+    img = PhotoImage(file="/Users/ujjwalraj/Desktop/bott.png")
+    panel = Label(i, image = img)
+    panel.pack(side = "bottom", fill = "both", expand = "yes")
+    
+func()

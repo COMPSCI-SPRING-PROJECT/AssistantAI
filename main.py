536bb5769b38b61e2ea03bb126755d440b221359
shahviransh <shahv47@mcmaster.ca>
2022-05-26 20:00:22 -0400

    basic googleSearch

--- main.py
+++ main.py
@@ -1,50 +1,70 @@
 from tkinter import *
+import webbrowser
 import speech_recognition as sr
 import pyttsx3
 import pyaudio
 import requests
+from datetime import datetime
 import time
 
-def main():    
-    voiceSpeech();
- 
+NAME = "Avery"
+
+def main():
+    time.sleep(1)
+    while 1:
+        respond(voiceSpeech())
+
 def speakText(command):
     engine = pyttsx3.init()
     newVoiceRate = 145
-    engine.setProperty('rate',newVoiceRate)
+    engine.setProperty('rate', newVoiceRate)
     engine.say(command)
     engine.runAndWait()
 
-def voiceSpeech():
+def voiceSpeech(ask=False):
     r = sr.Recognizer()
     mic = sr.Microphone()
-    
-    while(1):
-        try:
-            with mic as source:
-                r.adjust_for_ambient_noise(source, duration=0.2)
-                print("Listening: ")
-                audio = r.listen(source,timeout=2)
-                
-                myText = r.recognize_google(audio)
-                myText = myText.lower()
-                
-                if (myText == "stop"):
-                    print("Stop!")
-                    break
-    
-                print("Did you say "+ myText + "?")
-                speakText(myText)
-                
-        except sr.RequestError as e:
-            print("Could not request results; {0}".format(e))
-            
-        except sr.UnknownValueError:
-            print("Unknown error occured.")
+    try:
+        with mic as source:
+            if ask:
+                print(ask)
+                speakText(ask)
+            r.adjust_for_ambient_noise(source, duration=0.2)
+            print("Listening: ")
+            audio = r.listen(source, timeout=4)
+            myText = r.recognize_google(audio).lower()
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+    except sr.UnknownValueError:
+        print("Unknown error occured.")
+    return myText
 
-def googleSearch():
-    
+def respond(data):
+    if "what is your name" == data:
+        print("My name is " + NAME)
+        speakText("My name is " + NAME)
+    if "what" in data and "time" in data:
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+        print("Current Time =", current_time)
+        speakText("Current time is " + current_time)
+    if "find location" in data:
+        location = voiceSpeech("What is the location?")
+        url = "http://www.google.nl/maps/place/" + location + "/&amp;"
+        webbrowser.get().open(url)
+        print("Result for location " + location)
+        speakText("Result for location " + location)
+    if "search" in data:
+        search = voiceSpeech("What do you want to search for?")
+        print(search)
+        url = "http://www.google.com/search?q=" + search
+        webbrowser.get().open(url)
+        print("Result for " + search)
+        speakText("Result for " + search)
+    if "stop" in data or "exit" in data:
+        exit()
+
 def weather():
-    
+    print("Fill out here")
 
 main()

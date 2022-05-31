b13fb0ea0ea07bcd2e4dd761f2403fb2aaa2523a
leungt30 <leungt30@mcmaster.ca>
2022-05-31 12:32:05 -0400

    moved weather to own file for testing

---
+++ Weather_API.py
@@ -0,0 +1,41 @@
+import requests
+
+
+class Weather:
+
+    KEY = ""
+
+    def __init__(self, key):
+        self.KEY = key
+
+    def __callAPI(self, link):
+        req = requests.get(link)
+        data = req.json()
+        return data
+
+    def __convertKtoC(self, temp):
+        return round(temp-273.15, 1)
+
+    """
+    getWeather takes a location and returns a dictionary
+    the dictionary contains whether the API call was successful, if it is successful then it also 
+    contains weather description, current temperature, current feels like temperature, and humidity. 
+    """
+
+    def getWeather(self, location: str):
+        try:
+            geo_data = self.__callAPI(
+                "http://api.openweathermap.org/geo/1.0/direct?q="+location+"&limit=2&appid="+self.KEY)
+            lat = str(geo_data[0]['lat'])
+            lon = str(geo_data[0]['lon'])
+            api_data = self.__callAPI(
+                "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+self.KEY)
+            weather = api_data['weather'][0]['description']
+            temperature = str(self.__convertKtoC(
+                api_data['main']['temp']))+"°C"
+            feels_like = str(self.__convertKtoC(
+                api_data['main']['feels_like']))+"°C"
+            humidity = str(api_data['main']['humidity'])+"%"
+            return {'success': True, 'weather': weather, 'temperature': temperature, 'feels like': feels_like, 'humidity': humidity}
+        except Exception:
+            return {'success': False}
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import webbrowser
 import speech_recognition as sr
 import pyttsx3
-import requests
+import Weather_API
 import gtts
 import os
 from playsound import playsound
@@ -85,7 +85,7 @@ def respond(data):
         speakText("Result for " + search)
     elif "weather" in data:
         # Key in config.py, which is hidden due to .gitignore
-        weather = Weather(api_key)
+        weather = Weather_API.Weather(api_key)
         city = voiceSpeech("What city's weather do you want to search?")
         print(city)
         dict_weather = weather.getWeather(city)
@@ -101,46 +101,6 @@ def respond(data):
     else:
         print("Did you say" + data + "?")
         speakText("Did you say" + data + "?")
-
-
-class Weather:
-
-    KEY = ""
-
-    def __init__(self, key):
-        self.KEY = key
-
-    def __callAPI(self, link):
-        req = requests.get(link)
-        data = req.json()
-        return data
-
-    def __convertKtoC(self, temp):
-        return round(temp-273.15, 1)
-
-    """
-    getWeather takes a location and returns a dictionary
-    the dictionary contains whether the API call was successful, if it is successful then it also 
-    contains weather description, current temperature, current feels like temperature, and humidity. 
-    """
-
-    def getWeather(self, location: str):
-        try:
-            geo_data = self.__callAPI(
-                "http://api.openweathermap.org/geo/1.0/direct?q="+location+"&limit=2&appid="+self.KEY)
-            lat = str(geo_data[0]['lat'])
-            lon = str(geo_data[0]['lon'])
-            api_data = self.__callAPI(
-                "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+self.KEY)
-            weather = api_data['weather'][0]['description']
-            temperature = str(self.__convertKtoC(
-                api_data['main']['temp']))+"°C"
-            feels_like = str(self.__convertKtoC(
-                api_data['main']['feels_like']))+"°C"
-            humidity = str(api_data['main']['humidity'])+"%"
-            return {'success': True, 'weather': weather, 'temperature': temperature, 'feels like': feels_like, 'humidity': humidity}
-        except exception:
-            return {'success': False}
 
 
 main()
